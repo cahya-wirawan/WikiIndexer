550dd23e355be1cd9e0a200bf6970e01db442658
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2021-08-16 17:54:22 +0200

    update indexer

--- src/WIT.py
+++ src/WIT.py
@@ -175,7 +175,7 @@ descriptions_size = 0
 descriptions_test = []
 last_dataframe_size = 0
 
-random.seed(100)
+random.seed(1000)
 for wit_file in sorted(Path(WIT_dir).glob("*.tsv")):
     print(wit_file)
     descriptions, image_urls, index_map, dataframe_size = WIT_read(wit_file)
@@ -200,103 +200,3 @@ for i, value in enumerate(descriptions_test):
     assert (I[0][0] == value[1] or I[0][1] == value[1])
 
 exit(0)
-
-
-WIT_file = Path(WIT_dir)/Path(WIT_files[0])
-descriptions, image_urls, index_map, _ = WIT_read(WIT_file)
-print(len(descriptions))
-#exit(0)
-wit_dataset = {
-    "image_urls": image_urls,
-    "index_map": index_map
-}
-print("saving the wit_dataset")
-pickle.dump(wit_dataset, open("wit_dataset.pkl", "wb"))
-wit_dataset["descriptions"] = descriptions
-if len(sys.argv) == 2:
-    new_dimension = int(sys.argv[1])
-else:
-    new_dimension = 128
-model_name = f"distilbert-base-wiki-{new_dimension}"
-print(f"Model name: {model_name}")
-
-passages_length = len(descriptions)
-
-device = torch.device("cuda")
-
-if not os.path.exists(model_name):
-    model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
-
-    if torch.cuda.is_available():
-        model = model.to(device)
-    print(model.device)
-    #wiki_snippets_shuffled = wiki_snippets.shuffle(seed=42)
-    wiki_sentences = descriptions[:int(0.2*len(descriptions))]
-    pca_train_sentences = wiki_sentences
-    train_embeddings = model.encode(pca_train_sentences, convert_to_numpy=True, show_progress_bar=True)
-
-    print("Compute the PCA")
-    #Compute PCA on the train embeddings matrix
-    pca = PCA(n_components=new_dimension)
-    pca.fit(train_embeddings)
-    pca_comp = np.asarray(pca.components_)
-
-    dense = models.Dense(in_features=model.get_sentence_embedding_dimension(), out_features=new_dimension, bias=False, activation_function=torch.nn.Identity())
-    dense.linear.weight = torch.nn.Parameter(torch.tensor(pca_comp))
-    model.add_module('dense', dense)
-
-    model.save(model_name)
-    del(pca_train_sentences)
-    del(wiki_sentences)
-else:
-    model = SentenceTransformer(model_name)
-    if torch.cuda.is_available():
-        model = model.to(device)
-
-batch_size = 16000
-steps = passages_length//batch_size
-print("Start encoding the passages", passages_length, batch_size, steps)
-
-# Convert abstracts to vectors
-embeddings = None
-end = 0
-for step in tqdm(range(steps)):
-    start, end = step*batch_size, (step+1)*batch_size
-    if embeddings is None:
-        texts = descriptions[start:end]
-        embeddings = model.encode(texts, show_progress_bar=True)
-    else:
-        texts = descriptions[start:end]
-        embeddings = np.append(embeddings,
-                           model.encode(texts, show_progress_bar=True), axis=0)
-
-if end < passages_length:
-    embeddings = np.append(embeddings,
-                           model.encode(descriptions[end:passages_length], show_progress_bar=True), axis=0)
-print(embeddings.shape)
-wiki_ids = np.array(range(passages_length))
-
-# Step 1: Change data type
-# embeddings = np.array([embedding for embedding in embeddings]).astype("float32")
-
-# Step 2: Instantiate the index
-index = faiss.IndexFlatL2(embeddings.shape[1])
-
-# Step 3: Pass the index to IndexIDMap
-index = faiss.IndexIDMap(index)
-
-# Step 4: Add vectors and their IDs
-index.add_with_ids(embeddings, wiki_ids)
-
-print(f"Number of vectors in the Faiss index: {index.ntotal}")
-
-project_dir = Path('.').resolve()
-
-index_path = f"{project_dir}/wiki_faiss_{new_dimension}.idx"
-faiss.write_index(index, index_path)
-
-search_id = 30
-id = faiss.read_index(index_path)
-D, I = id.search(np.array([embeddings[search_id]]), k=10)
-print(f'L2 distance: {D.flatten().tolist()}\n\nMAG paper IDs: {I.flatten().tolist()}')
-print(f"text: << {descriptions[search_id]} >>\nurl: {image_urls[index_map[search_id]]}")
